c777ea970cc0d12e9ffc4fafec2a5d947f382c0d
Osemwengie Arit Bright <uyearit23@gmail.com>
2026-07-21 08:30:47 -1200

    Add quiz questions data

---
+++ script.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks and Text Markup Language",
+      "Hyper Transfer Markup Language",
+    ],
+    correct: 0,
+  },
+  {
+    question: "Which language is used to style web pages?",
+    answers: ["HTML", "Python", "CSS", "Java"],
+    correct: 2,
+  },
+  {
+    question: "Which language is used to make web pages interactive?",
+    answers: ["CSS", "JavaScript", "PHP", "SQL"],
+    correct: 1,
+  },
+  {
+    question: "Which company developed JavaScript?",
+    answers: ["Microsoft", "Apple", "Netscape", "Google"],
+    correct: 2,
+  },
+  {
+    question: "Which HTML tag is used to create a hyperlink?",
+    answers: ["<img>", "<a>", "<link>", "<button>"],
+    correct: 1,
+  },
+];
